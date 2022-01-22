ff4855c6f2ee4a70dcc08637e3187dd97dd4a5bd
RuverQ <52637077+RuverQ@users.noreply.github.com>
2022-01-22 15:35:20 +0300

    + Wave progress bar effect
    HOW I DID THAT WTFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF

--- Source/MystCore/Weapons/DefaultUnrealGun.cpp
+++ Source/MystCore/Weapons/DefaultUnrealGun.cpp
@@ -28,6 +28,9 @@ void ADefaultUnrealGun::Tick(float DeltaTime)
 
 void ADefaultUnrealGun::Fire()
 {
+	if(CharacterOwner->ManaComponent->Mana <= 100) return;
+	CharacterOwner->ManaComponent->SuckMana(100);
+	
 	Super::Fire();
 }
 
