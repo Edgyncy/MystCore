1a5a01e59415bc07a4ac513baf5d9694f0d8bb92
RuverQ <52637077+RuverQ@users.noreply.github.com>
2022-02-06 23:30:58 +0300

    Second RoboArms Test

--- Source/MystCore/MystCoreGameMode.cpp
+++ Source/MystCore/MystCoreGameMode.cpp
@@ -9,9 +9,9 @@ AMystCoreGameMode::AMystCoreGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Characters/FirstPerson/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Characters/FirstPerson/Blueprints/FirstPersonCharacter"));
+	//DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
-	HUDClass = AMystCoreHUD::StaticClass();
+	//HUDClass = AMystCoreHUD::StaticClass();
 }
